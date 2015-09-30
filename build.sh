d42e2cd7ee099528b486e59ca40fa187e6f464dd
Romain Commandé <commande.romain@gmail.com>
2015-09-30 23:39:57 +0200

    Tag previous docker image

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 HERE=$(pwd)
+
 pip install pelican
 pelican-themes -s $HERE/themes/Colourise11
 make html
+
+# Tag the old version
+docker pull rcommande/blog
+docker tag -f rcommande/blog rcommande/blog:previous
+docker push rcommande/blog
+
+# Create the new image
 docker build -t rcommande/blog .
 docker push rcommande/blog
